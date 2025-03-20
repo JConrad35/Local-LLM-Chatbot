6c809e4debe09d24bc477f0bc15c4797ba96383a
Julian Conrad <julianhconrad01@gmail.com>
2025-03-19 21:48:07 -0600

    delete

--- chatbot_app.py
+++ chatbot_app.py
@@ -1,24 +1,40 @@
 from langchain_core.prompts import ChatPromptTemplate
-from langchain_ollama.llms import OllamaLLM
+from langchain_ollama import OllamaLLM
+import os
 
+# Function to search for files
+def search_files(name, path = "C:\Users"): # might have to add a path to variable 'path'
+    result = []
+    for root, dirs, files in os.walk(path):
+        if name in files:
+            result.append(os.path.join(root, name))
+            return result
+
+
+# Chat Prompt Template
 template = ChatPromptTemplate([
-    ('system', 'You are a helpful AI assistant. Your name is {name}.'),
-    ('human', 'Hello, how are you?'),
-    ('ai', 'I am doing well, thank you. How can I help you today?'),
-    ('human', '{user_input}')
+    ("system", 
+     """"
+     You are an ai assistant that helps users find files on their computer. These are your instructions:
+        1. The user will provide you with a file name.
+        2. You will search the user's computer for the file.
+        3. If the file is found, you will return the file path.
+        4. If the file is not found, you will let the user know.
+     """),
+    ("human", "Help me find the file {file_name}."),
+    ("ai", "I can help you find the file {file_name}."),
+    ("system", "Searching for file {file_name}."),
+    ("ai", "I found the file '{file_name}' at the following path: {file_path}."),
+    ("ai", "I'm sorry, I couldn't find the file '{file_name}'."),
+    ("human", "Thank you."),
+    ("ai", "You're welcome. Let me know if you need to find another file."),
 ])
-
-prompt_value = ChatPromptTemplate.from_template(template)
 
 model = OllamaLLM(model="llama3.2")
 
-chain = prompt_value | model
+chain = template | model
 
-response = template.invoke(
-    {
-        'name': 'Juju',
-        'user_input': 'What is your name?'
-    }
-)
-
-print(response)
+chain.invoke({
+    "file_name": "example.txt",
+    "file_path": "C:/Users/username/Documents/example.txt"
+})
